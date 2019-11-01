076a1436aea8b1f41139bf486bb8709bf6fe5567
Bernadette Davis <bsd231@nyu.edu>
2019-11-01 19:03:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,16 +18,25 @@ class ApplicationController < Sinatra::Base
     redirect to '/recipes/' + recipe.id.to_s
   end
 
+<<<<<<< HEAD
   get '/recipes/new' do
     erb :new
   end
 
+=======
+>>>>>>> b910590ac5a957571513ce82917d34fe6e52c2d2
   get '/recipes/:id' do
     @recipe = Recipe.all.find(params[:id])
     erb :show
   end
 
+<<<<<<< HEAD
 
+=======
+  get '/recipes/new' do
+    erb :new
+  end
+>>>>>>> b910590ac5a957571513ce82917d34fe6e52c2d2
 
   get '/recipes/:id/edit' do
     @recipe = Recipe.all.find(params[:id])
